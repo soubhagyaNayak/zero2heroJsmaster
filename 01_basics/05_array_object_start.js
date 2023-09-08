562c027f92f4bb6927115752ece298d5668395dd
Soubhagya Ranjan Nayak <45428643+soubhagyaNayak@users.noreply.github.com>
2023-09-08 14:58:52 +0000

    basic of array and object

---
+++ 01_basics/05_array_object_start.js
@@ -0,0 +1,19 @@
+//object always declared in the form of key value pair
+//object creating format
+const mydetails={
+    name:"soubhagya",
+    rollNumber: 1234,
+    password:"abc@134"
+}
+console.log(typeof mydetails)
+
+//in the array we can store object,string,number etc
+//everything in a js is an object
+const mypro=[
+    "soubhgaya",234,{
+        name:"soubhagya",
+        rollNumber:"1234",
+        class:"MCA",
+    }
+]
+console.log(typeof mypro)
