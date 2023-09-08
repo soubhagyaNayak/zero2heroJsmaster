9b796efdba0060edf9c47d449cb911faf74e6f6f
Soubhagya Ranjan Nayak <45428643+soubhagyaNayak@users.noreply.github.com>
2023-09-08 10:12:27 +0000

    datatypes kii kahanii

---
+++ 01_basics/second.js
@@ -0,0 +1,21 @@
+// datatypes
+
+let score=33;
+let name="bibek"
+let value=true
+let vivek;
+let sona=null;
+//it gives number datatypes
+// console.log(typeof score)
+
+//gives boolean datatypes
+// console.log(typeof value)
+
+//it gives value undefined
+// console.log(typeof vivek)
+
+//null is a object
+// console.log(typeof sona)
+let score2="33abc"
+console.log( typeof score2)
+
