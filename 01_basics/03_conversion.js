a70d3513fa76c314d35ad3bfe93a9274e1985365
Soubhagya Ranjan Nayak <45428643+soubhagyaNayak@users.noreply.github.com>
2023-09-08 14:58:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score ="33"
+console.log(typeof (score))
+
+//String types
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber)
+
+//Number datatypes
+let score2 ="33bgr"
+console.log(score2)
+let voka=Number(score2)
+console.log(voka)
+
+
+//number to string conversion
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
+
+//Boolean
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
